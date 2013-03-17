d1a7b53209e9ffe30620fb35bc84d631c57afd97
Matthew Rasmussen <mrasmus@gmail.com>
2013-03-17 03:33:59 -0700

    Proper bridging functionality now, based on env variables.

--- PyIRCBridge.py
+++ PyIRCBridge.py
@@ -7,6 +7,8 @@ import os
 from Queue import Queue
 from threading import Thread
 from time import sleep
+from json import loads, dumps
+from re import search, split, sub
 
 IDENT='CBA Testbot'
 REALNAME='Cloudboat Armada'
@@ -19,17 +21,22 @@ def sendtosocket(s,msg):
 	s.send(msg+EOL)
 
 class Server(Thread):
-	def __init__(self,host,channel,nick='cbirc',port=6667,**kwargs):
+	def __init__(self,name,host,channels,nick='cbirc',port=6667,serverpass=None,nspass=None,**kwargs):
+		self.servername = name
 		self.host = host
+		self.serverpass = serverpass
+		self.nspass = nspass
 		self.port = port
 		self.nick = nick
-		self.channel = channel
+		self.channels = channels
 		self.s = socket()
 		self.targets = []
 		super(Server, self).__init__(**kwargs)
 
 	def run(self):
 		self.s.connect((self.host, self.port))
+		if (self.serverpass is not None):
+			sendtosocket(self.s, 'PASS '+self.serverpass)
 		sendtosocket(self.s,'NICK '+self.nick)
 		sendtosocket(self.s,'USER '+IDENT+' '+self.host+' bla :'+REALNAME)
 		done = False
@@ -39,39 +46,56 @@ class Server(Thread):
 			if line == '':
 				done = True
 				print '****Socket on ' + self.host + ' closed.'
-			elif line.find('NickServ') != -1:
-				sendtosocket(self.s,'JOIN '+self.channel)
-				print '****Joined channel '+self.channel+' on '+self.host
+			if line.find('NOTICE ' + self.nick + ' :This nickname is registered.')!=-1:
+				print "Identify to nickserv...\n"
+				sendtosocket(self.s,'PRIVMSG NickServ IDENTIFY '+self.nspass+'\r\n')
+			if (line.find('You are now identified for') != -1) or (line.find('End of /MOTD command') != -1):
+				print '**************JOINING CHANNELS ON ' + self.host
+				for chan in self.channels:
+					sendtosocket(self.s,'JOIN '+chan)
+					print '****Joined channel '+chan+' on '+self.host
 			elif (line.split()[0]=='PING'):
 				sendtosocket(self.s,'PONG '+line.rstrip().split()[1])
 				print '****Sending:'+'PONG '+line.rstrip().split()[1][1:]
 			else:
-				for t in self.targets:
-					t.put(line)
+				for l in [l for l in split('\r\n',line) if 'PRIVMSG #' in l]:
+					m = search(':(.*?)!.* PRIVMSG (#[^ ,]+) :(.+)',l)
+					msg = '{'+m.group(1)+'} '+m.group(3)
+					print "Message on " + self.servername + m.group(2) + ": " + msg
+					for t in self.targets:
+						t.put(msg)
 
 class Repeater(Thread):
+	def __init__(self,sock,chan,**kwargs):
+		self.q = Queue()
+		self.s = sock
+		self.channel = chan
+		super(Repeater, self).__init__(**kwargs)
+
 	def run(self):
 		while True:
 			sendtosocket(self.s,'PRIVMSG '+self.channel+' :'+self.q.get()+EOL)
 			print 'Message sent to '+self.channel
 
-server1=Server('irc.chatspike.net','#cbatest')
-server2=Server('irc.chatspike.net','#cbatest1',nick='cbirc1')
-repeater1 = Repeater()
-repeater1.q = Queue()
-repeater1.s = server2.s
-repeater1.channel = server2.channel
-server1.start()
-server2.start()
-repeater1.start()
+servers = {} 
+srvdict = loads(os.environ["IRCSERVERS"])
+for key in srvdict.keys():
+	srv=srvdict[key]
+	servers[key] = Server(key,srv['host'],srv['channels'],srv['nick'],srv['port'],srv['serverpass'],srv['nspass'])
+for server in servers.values():
+	server.start()
 
 sleep(5)
-server1.targets+=[repeater1.q]
-print 'Queue following server1, repeated to server2'
+
+for sub in loads(os.environ['IRCSUBS']):
+	print 'sending messages from ' + servers[sub[1]].servername + ' to '+servers[sub[0]].servername + sub[2]
+	r = Repeater(servers[sub[0]].s,sub[2])
+	r.start()
+	servers[sub[1]].targets+=[r.q]
 
 while True:
-	x = 0
 	#print '++++Received line: ||' + q1.get()
+	pass
 
 HOST='irc.chatspike.net' #The server we want to connect to
 PORT=6667 #The connection port which is usually 6667
